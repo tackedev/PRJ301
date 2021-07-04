3bde739bac59a620e98f74fb9269eca7599435b2
tackedev <quangky.it@gmail.com>
2021-07-04 20:13:16 +0700

    add DispatcherFiler

---
+++ LearningProjectFinal/src/java/kylq/filter/DispatcherFilter.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kylq.filter;
+
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tackedev
+ */
+public class DispatcherFilter implements Filter {
+    
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public DispatcherFilter() {
+    }    
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        // set some header for responseObj
+        httpResponse.setContentType("text/html;charset=UTF-8");
+        httpResponse.setHeader("Control-cache", "no-cache, no-store");
+        
+        //get roadmap from Application Scope
+        ServletContext context = httpRequest.getServletContext();
+        Map<String, String> roadmap = (Map<String, String>) context.getAttribute("ROAD_MAP");
+        
+        //get resource
+        //getServletPath() return "/servletName"
+        String action = httpRequest.getServletPath().substring(1);
+        String resource = roadmap.get(action);
+        
+        if (resource != null) {
+            RequestDispatcher rd = httpRequest.getRequestDispatcher(resource);
+            rd.forward(request, response);
+        } else {
+            httpResponse.sendError(httpResponse.SC_NOT_FOUND); // send 404 Page not found error
+        }
+        
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("DispatcherFilter()");
+        }
+        StringBuffer sb = new StringBuffer("DispatcherFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+}
