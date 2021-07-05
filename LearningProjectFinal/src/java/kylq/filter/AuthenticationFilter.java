427d1fd5eb0c3f6e59b704d0e1c189eff2537ad7
tackedev <quangky.it@gmail.com>
2021-07-05 10:20:26 +0700

    add AutenticationFilter

---
+++ LearningProjectFinal/src/java/kylq/filter/AuthenticationFilter.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kylq.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author tackedev
+ */
+public class AuthenticationFilter implements Filter {
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public AuthenticationFilter() {
+    }    
+    
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        //check existed session and existed authenticated user
+        HttpSession session = httpRequest.getSession(false);
+        if (session == null || session.getAttribute("USER") == null) {
+            httpResponse.sendRedirect("login");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     * @return 
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     * @param filterConfig
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("AuthenticationFilter()");
+        }
+        StringBuffer sb = new StringBuffer("AuthenticationFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+    
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);        
+    }
+    
+}
