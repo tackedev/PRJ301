1eb0f32f34d7eb434e320b8ca23381ae07a12502
tackedev <quangky.it@gmail.com>
2021-07-06 18:03:51 +0700

    add Validation for Edit Account

--- LearningProjectFinal/src/java/kylq/controller/ConfirmEditServlet.java
+++ LearningProjectFinal/src/java/kylq/controller/ConfirmEditServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import kylq.registration.RegistrationDTO;
+import kylq.registration.RegistrationInsertError;
 
 /**
  *
@@ -33,6 +34,8 @@ public class ConfirmEditServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        String url = "editAccountPage";
+        
         String username = request.getParameter("txtUsername");
         String password = request.getParameter("txtPassword");
         String lastName = request.getParameter("txtFullName");
@@ -44,10 +47,31 @@ public class ConfirmEditServlet extends HttpServlet {
         HttpSession session = request.getSession();
         session.setAttribute("EDIT_USER", dto);
         
+        //validate edit input
+        //password (6 - 30 chars)
+        //fulllname (2 - 50 chars)
+        RegistrationInsertError errors = new RegistrationInsertError();
+        boolean foundErrors = false;
+        if (password.length() < 6 || 30 < password.length()) {
+            foundErrors = true;
+            errors.setPasswordLengthErr("Password is required from 6 to 30 characters");
+        }
+        if (lastName.length() < 2 || 50 < lastName.length()) {
+            foundErrors = true;
+            errors.setFullNameLengthErr("Fullname is required from 2 to 50 characters");
+        }
+        
+        if (foundErrors) {
+            //set errors to request scope
+            request.setAttribute("INSERT_ERRORS", errors);
+        } else {
+            url = "confirmEditPage";
+        }
+        
         //get roadmap form application scope
         Map<String, String> roadmap = (Map<String, String>) request.getServletContext().getAttribute("ROAD_MAP");
         //forward to confirmEditPage
-        RequestDispatcher rd = request.getRequestDispatcher(roadmap.get("confirmEditPage"));
+        RequestDispatcher rd = request.getRequestDispatcher(roadmap.get(url));
         rd.forward(request, response);
     }
 
